f3e0338fd6c21b43995bb289259668f6d37cede1
Chad Sharp <crossroads1112@gmail.com>
2014-08-27 20:44:42 -0400

    Added option ot install pacman_pkgs and aur_pkgs

--- make.sh
+++ make.sh
@@ -29,3 +29,30 @@ for file in $files; do
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
+#Extra stuff
+echo "Setting up mpd for user $(whoami)"
+mkdir -p ~/.config/mpd/playlists
+touch ~/.config/mpd/{database,log,pid,state,sticker,sql}
+cp $dir/mpd.conf ~/.config/mpd/mpd.conf
+
+echo "Do you want to install your previous packages?"
+read answer
+case $answer in 
+    ""|[Yy]|[Yy][Ee][Ss]) sudo pacman -S --needed $(comm -12 <(pacman -Slq|sort) <(sort $dir/pacman_pkgs))
+        ;;
+    [Nn]|[Nn][Oo]) exit 0
+        ;;
+    *) echo "Sorry, that's not an acceptable response"
+        ;;
+esac
+
+
+echo "Do you want to install AUR packages as well? [y/N]"
+case $answer in 
+    [Yy]|[Yy][Ee][Ss]) yaourt -S --needed $(comm -13 <(pacman -Slq|sort) <(sort $dir/aur_pkgs))
+        ;;
+    ""|[Nn]|[Nn][Oo])  exit 0 
+        ;;
+    *) echo "Sorry, that's not an acceptable response"
+        ;;
+esac
