d2fe6c2caa75d901310bce604e295329e0cabc65
Chad Sharp <crossroads1112@gmail.com>
2015-05-14 20:38:56 -0400

    Only move vim directory instead of link

--- setup.sh
+++ setup.sh
@@ -1,23 +1,11 @@
 dir=~/.dotfiles # dotfiles directory
 olddir=~/.dotfiles_old # old dotfiles backup directory
-files="zshrc vimrc tmux.conf vimperatorrc zsh_plugins mailcap mutt muttrc" # list of files/folders to symlink in homedir
-dotfilesrepo="crossroads1112/dotfiles"
-
+lnfiles="zshrc vimrc tmux.conf vimperatorrc zsh_plugins mailcap mutt muttrc" # list of files/folders to symlink in homedir
+mvfiles="vim"
 bold(){
     echo "$(tput bold)$@$(tput sgr0)"
 }
 
-if [[ ! -d $dir || ! -d $dir/.git ]]; then
-    bold "Either dotfiles directory does not exist or there is no git repo there. Moving to ${dir}.bak"
-    mv $dir ${dir}.bak
-    mkdir -p $dir
-    cd $dir
-    bold "Setting up git repo"
-    git init
-    git remote add origin-https https://github.com/$dotfilesrepo
-    git remote add origin git@github.com:$dotfilesrepo 
-    git pull origin-https master
-fi
 bold "Decrypting mutt passwords"
 scrypt enc $dir/mypw.gpg.bfe > ~/.mypw.gpg
 
@@ -29,9 +17,13 @@ bold "Changing to the $dir directory ..."
 cd $dir
 bold "Done"
 
-for file in $files; do
+for lnfile in $lnfiles; do
     bold "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file $olddir/
-    bold "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    mv ~/.$lnfile $olddir/
+    bold "Creating symlink to $lnfile in home directory."
+    ln -s $dir/$lnfile ~/.$lnfile
+done
+for mvfile in $mvfiles; do
+    bold "Moving $mvfile from $dir/$mvfile to ~/.$mvfile"
+    mv "$dir/mvfile" "~/.$mvfile"
 done
