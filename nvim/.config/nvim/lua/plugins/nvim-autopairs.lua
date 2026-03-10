8c724c19daa0551ed3d270d28eabad2746c8e139
Chad Sharp <cmlsharp@umich.edu>
2026-03-11 06:23:44 +0800

    notification center style

--- nvim/.config/nvim/lua/plugins/nvim-autopairs.lua
+++ nvim/.config/nvim/lua/plugins/nvim-autopairs.lua
@@ -1,8 +1,9 @@
 return {
   "windwp/nvim-autopairs",
   event = "InsertEnter",
-  config = function(plugin)
+  config = function()
+    local plugin = require "nvim-autopairs"
+    plugin.setup {}
     plugin.get_rule("'")[1].not_filetypes = { "ocaml" }
-    plugin.get_rule("`")[1].not_filetypes = { "lean" }
   end,
 }
